289fb927a8b42aba115872942537edf2e05935f2
Markus Schulz <schulz@alpharesearch.de>
2025-07-03 11:27:55 -0400

    Feat: Update README for environment setup and modify app configuration

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from opennourish import create_app
 
-# This function is automatically discovered by Flask's CLI
-def create_app_cli():
-    return create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
