3b3e985e1e4bd949ed3b7cc34b69da5673637381
Markus Schulz <schulz@alpharesearch.de>
2025-07-10 21:36:05 -0400

    fix: favicon

--- opennourish/main/routes.py
+++ opennourish/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, send_file, current_app, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for, send_from_directory, current_app, jsonify
 from flask_login import current_user
 from models import db, Food, UnifiedPortion
 from sqlalchemy.orm import sessionmaker
@@ -7,6 +7,11 @@ import tempfile
 import os
 
 main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(current_app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @main_bp.route('/')
 def index():
