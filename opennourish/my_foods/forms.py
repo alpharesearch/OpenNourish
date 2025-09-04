56cac55bb7c8a622f5d2b07f92ca8c97a0a8d688
Markus Schulz <schulz@alpharesearch.de>
2025-09-03 21:02:51 -0400

    refactor: Remove default values for nutritional fields in MyFoodForm

--- opennourish/my_foods/forms.py
+++ opennourish/my_foods/forms.py
@@ -18,67 +18,57 @@ class MyFoodForm(FlaskForm):
     calories_per_100g = FloatField(
         "Calories (cal per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     protein_per_100g = FloatField(
-        "Protein (g per 100g)", validators=[Optional(), NumberRange(min=0)], default=0.0
+        "Protein (g per 100g)", validators=[Optional(), NumberRange(min=0)]
     )
     carbs_per_100g = FloatField(
         "Carbohydrates (g per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     fat_per_100g = FloatField(
-        "Fat (g per 100g)", validators=[Optional(), NumberRange(min=0)], default=0.0
+        "Fat (g per 100g)", validators=[Optional(), NumberRange(min=0)]
     )
     saturated_fat_per_100g = FloatField(
         "Saturated Fat (g per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     trans_fat_per_100g = FloatField(
         "Trans Fat (g per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     cholesterol_mg_per_100g = FloatField(
         "Cholesterol (mg per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     sodium_mg_per_100g = FloatField(
-        "Sodium (mg per 100g)", validators=[Optional(), NumberRange(min=0)], default=0.0
+        "Sodium (mg per 100g)", validators=[Optional(), NumberRange(min=0)]
     )
     fiber_per_100g = FloatField(
-        "Fiber (g per 100g)", validators=[Optional(), NumberRange(min=0)], default=0.0
+        "Fiber (g per 100g)", validators=[Optional(), NumberRange(min=0)]
     )
     sugars_per_100g = FloatField(
         "Total Sugars (g per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     added_sugars_per_100g = FloatField(
         "Added Sugars (g per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     vitamin_d_mcg_per_100g = FloatField(
         "Vitamin D (mcg per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     calcium_mg_per_100g = FloatField(
         "Calcium (mg per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     iron_mg_per_100g = FloatField(
-        "Iron (mg per 100g)", validators=[Optional(), NumberRange(min=0)], default=0.0
+        "Iron (mg per 100g)", validators=[Optional(), NumberRange(min=0)]
     )
     potassium_mg_per_100g = FloatField(
         "Potassium (mg per 100g)",
         validators=[Optional(), NumberRange(min=0)],
-        default=0.0,
     )
     submit = SubmitField("Save Food")
 
